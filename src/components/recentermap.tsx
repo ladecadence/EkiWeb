e1106b14998e67948b2119b112b0091070139b03
David Pello <zako@ladecadence.net>
2025-05-12 16:24:25 +0200

    Use map events to calculate offset (to center with offset for the popup)

--- src/components/recentermap.tsx
+++ src/components/recentermap.tsx
@@ -1,15 +1,26 @@
 import { LatLngTuple } from "leaflet";
-import { useEffect } from "react";
-import { useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useMap, useMapEvents } from "react-leaflet";
 
 interface recenterProps {
     position: LatLngTuple;
 }
 
 const RecenterAutomatically = ({ position }: recenterProps) => {
+    const [zoom, setZoom] = useState(15);
     const map = useMap();
+
+    // manage zoom
+    const mapEvents = useMapEvents({
+        zoomend: () => {
+            setZoom(mapEvents.getZoom());
+        },
+    });
+
     useEffect(() => {
-        map.setView(position);
+        var targetPoint = map.project(position, zoom).subtract([0, 50]),
+        targetLatLng = map.unproject(targetPoint, zoom);
+        map.setView(targetLatLng);
     }, position);
     return null;
 }
